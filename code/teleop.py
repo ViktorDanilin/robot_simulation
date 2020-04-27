bee398049e638ac357e36593c95bc357c1210f74
ViktorDanilin <danilinvs58@gmail.com>
2020-04-27 23:41:47 +0700

    hello world

--- code/teleop.py
+++ code/teleop.py
@@ -7,24 +7,43 @@ from std_msgs.msg import UInt16
 from std_msgs.msg import UInt32
 
 def on_press(key):
-    global forvard,back,left,right,vel_msg,serv_msg,pub1,pub2,pub3
+    global forvard,back,left,right,vel_msg,serv_msg,motor_msg,slider_msg,pub1,pub2,pub3,pub4,pub5
     if not rospy.is_shutdown():
-
+        if (key == KeyCode(char='n')):
+            motor_msg = 0
+            #стоп крутилка
+            pub4.publish(motor_msg)
+        elif (key == KeyCode(char='b')):
+            motor_msg = 1
+            #влево крутилка
+            pub4.publish(motor_msg)
+        elif (key == KeyCode(char='m')):
+            motor_msg = 2
+            #вправо крутилка
+            pub4.publish(motor_msg)
+        if (key == KeyCode(char='z')):
+            slider_msg = 0
+            #слайдер вперед
+            pub5.publish(motor_msg)
+        elif (key == KeyCode(char='x')):
+            slider_msg = 1
+            #слайдер назад
+            pub5.publish(motor_msg)
         if (key == KeyCode(char='w')):
             vel_msg.linear.x = forvard
-            print('go forvard')
+            #вперед
             pub1.publish(vel_msg)
         elif (key == KeyCode(char='a')):
             vel_msg.angular.z = left
-            print('go left')
+            #влево
             pub1.publish(vel_msg)
         elif (key == KeyCode(char='d')):
             vel_msg.angular.z = right
-            print('go right')
+            #вправо
             pub1.publish(vel_msg)
         elif (key == KeyCode(char='s')):
             vel_msg.linear.x = back
-            print('go back')
+            #назад
             pub1.publish(vel_msg)
         elif (key == KeyCode(char='t')):
             vel_msg.linear.x = 0
@@ -33,13 +52,13 @@ def on_press(key):
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
             vel_msg.angular.z = 0
-            print('stop')
+            #стоп
             pub1.publish(vel_msg)
         elif (key == Key.esc):
             exit()
             
 def main(key):
-    global forvard, back, left, right,vel_msg,serv_msg,pub1,pub2,pub3
+    global forvard, back, left, right,vel_msg,motor_msg,slider_msgserv_msg,pub1,pub2,pub3,pub4,pub5
     forvard = 3
     back = -3
     right = -2
@@ -53,8 +72,8 @@ def main(key):
     pub3 = rospy.Publisher("/arduino/servo2", UInt16, queue_size=10)
     pub4 = rospy.Publisher("/arduino/motor1", UInt32, queue_size=10)
     motor_msg = UInt32
-    pub5 = rospy.Publisher("/arduino/slider",UInt16, queue_size=10)
-    slider_msg = UInt16
+    pub5 = rospy.Publisher("/arduino/slider",UInt32, queue_size=10)
+    slider_msg = UInt32
     with Listener(on_press=on_press) as listener:
         listener.join()
             
