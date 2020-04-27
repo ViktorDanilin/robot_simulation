87aec365c5b85675fd581b60e15517d6e270535f
ViktorDanilin <danilinvs58@gmail.com>
2020-04-27 18:52:45 +0700

    hello world

--- code/key.py
+++ code/key.py
@@ -1,23 +1,55 @@
-from pynput.keyboard import Key, Listener, KeyCode, Key
+#!/usr/bin/env python3
 
-class Keyboard():
-    def on_press(self, key):
-        #print('{0} pressed'.format(key))
+import rospy
+from pynput.keyboard import Key, Listener, KeyCode, Key
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int16
+
+
+def main(key):
+
+    forvard = 3
+    back = -3
+    right = -2
+    left = 2
+
+    rospy.init_node("joystick", anonymous=True)
+    pub1 = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    vel_msg = Twist()
+    pub2 = rospy.Publisher("/arduino/servo1", Int16, queue_size=10)
+    serv_msg = Int16
+    pub3 = rospy.Publisher("/arduino/servo2", Int16, queue_size=10)
+
+    while not rospy.is_shutdown():
         if (key == KeyCode(char='w')):
+            vel_msg.linear.x = forvard
             print('go forward')
+            pub1.publish(vel_msg)
         elif (key == KeyCode(char='a')):
+            vel_msg.linear.x = left
             print('go left')
+            pub1.publish(vel_msg)
         elif (key == KeyCode(char='d')):
+            vel_msg.linear.x = right
             print('go right')
+            pub1.publish(vel_msg)
         elif (key == KeyCode(char='s')):
+            vel_msg.linear.x = back
             print('go back')
+            pub1.publish(vel_msg)
         elif (key == KeyCode(char='t')):
-            print('stop')  
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = 0
+            print('stop')
+            pub1.publish(vel_msg)
         elif (key == Key.esc):
             exit()
-    def __init__(self):
-        with Listener(on_press=self.on_press) as listener:
-            listener.join()
-       
 
-Keyboard()
+try:
+    main(Key)
+except rospy.ROSInterruptException:
+    pass
--- code/keyboard.py
+++ code/keyboard.py
@@ -10,18 +10,17 @@ forvard = 3
 back = -3
 right = -2
 left = 2
-class Keyboard():
-    def __init__(self):
-        rospy.init_node("joystick", anonymous=True)
-        pub1 = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-        vel_msg = Twist()
-        pub2 = rospy.Publisher("/arduino/servo1", Int16, queue_size=10)  # servo up/down
-        serv_msg = Int16
-        pub3 = rospy.Publisher("/arduino/servo2", Int16, queue_size=10)  # servo grab
-        self.last = None
-    # Функция считываеия с клавиатуры        
-    def on_press(self, key):
-        #print('{0} pressed'.format(key))
+    # Функция считываеия с клавиатуры
+
+def main(self, key):
+
+    rospy.init_node("joystick", anonymous=True)
+    pub1 = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    vel_msg = Twist()
+    pub2 = rospy.Publisher("/arduino/servo1", Int16, queue_size=10)  # servo up/down
+    serv_msg = Int16
+    pub3 = rospy.Publisher("/arduino/servo2", Int16, queue_size=10)  # servo grab
+    while not rospy.is_shutdown():
         if key == Key.up: # Поднятие серв 1 или 2
             if self.last == KeyCode(char='q'):
                 print('servo 1 up')
@@ -31,7 +30,7 @@ class Keyboard():
             if self.last == KeyCode(char='q'):
                 print('servo 1 down')
             elif self.last == KeyCode(char='r'):
-                print('servo 2 down')        
+                print('servo 2 down')
         elif (key == KeyCode(char='w')): # Проезд вперед
             vel_msg.linear.x = forvard
             print('go forward')
@@ -39,40 +38,38 @@ class Keyboard():
         elif (key == KeyCode(char='a')): # Проезд влево
             vel_msg.angular.z = left
             print('go left')
-            pub1.publish(vel_msg)
-        elif (key == KeyCode(char='d')): # Проезд вправо
-            vel_msg.angular.z = right
-            print('go right')
-            pub1.publish(vel_msg)
-        elif (key == KeyCode(char='s')): # Проезд назад
-            vel_msg.linear.x = back
-            print('go back')
-            pub1.publish(vel_msg)
-        elif (key == KeyCode(char='t')): # Остановка ровера
-            vel_msg.linear.x = 0
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = 0
-            print('stop')
-            pub1.publish(vel_msg)
-        elif (key == KeyCode(char='q')): # Включение режима работы с сервой 1
-            self.last = key
-            print('servo 1')
-        elif (key == KeyCode(char='e')): # Включение режима работы с nfs
-            self.last = key
-            print('nfs')
-        elif (key == KeyCode(char='r')): # Включение режима работы с сервой 2
-            self.last = key
-            print('servo 2')        
-        elif (key == Key.esc): # Выход из программы
-            exit()
-    def spin(self): # Утилита для общения с топиками
-        rospy.spin()
+        pub1.publish(vel_msg)
+    elif (key == KeyCode(char='d')): # Проезд вправо
+        vel_msg.angular.z = right
+        print('go right')
+        pub1.publish(vel_msg)
+    elif (key == KeyCode(char='s')): # Проезд назад
+        vel_msg.linear.x = back
+        print('go back')
+        pub1.publish(vel_msg)
+    elif (key == KeyCode(char='t')): # Остановка ровера
+        vel_msg.linear.x = 0
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 0
+        print('stop')
+        pub1.publish(vel_msg)
+    elif (key == KeyCode(char='q')): # Включение режима работы с сервой 1
+        self.last = key
+        print('servo 1')
+    elif (key == KeyCode(char='e')): # Включение режима работы с nfs
+        self.last = key
+        print('nfs')
+    elif (key == KeyCode(char='r')): # Включение режима работы с сервой 2
+        self.last = key
+        print('servo 2')
 
+def __init__(self):
+    with Listener(main=self.on_press) as listener:
+        listener.join()
 try:
-    k = Keyboard()
-    k.spin() # Запуск программы
+    main(self,key)
 except rospy.ROSInterruptException:
     pass
