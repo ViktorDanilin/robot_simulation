1b0b17e8dae837872546ae6ec4ef300618e3536a
ViktorDanilin <danilinvs58@gmail.com>
2020-04-28 13:38:04 +0700

    Hello world

--- code/teleop.py
+++ code/teleop.py
@@ -25,16 +25,16 @@ def on_press(key):
             slider_msg = 1
             pub5.publish(slider_msg)
         if (key == KeyCode(char='p')):
-            serv1_msg += 20
+            serv1_msg = 170
             pub2.publish(serv1_msg)
         elif (key == KeyCode(char='l')):
-            serv1_msg -= 20
+            serv1_msg = 10
             pub2.publish(serv1_msg)
         if (key == KeyCode(char='i')):
-            serv2_msg += 20
+            serv2_msg = 170
             pub3.publish(serv2_msg)
         elif (key == KeyCode(char='j')):
-            serv2_msg -= 20
+            serv2_msg = 10
             pub3.publish(serv2_msg)
         if (key == KeyCode(char='w')):
             vel_msg.linear.x = forvard
@@ -61,8 +61,8 @@ def on_press(key):
             
 def main(key):
     global forvard, back, left, right, vel_msg, motor_msg, slider_msg, serv1_msg, serv2_msg, pub1, pub2, pub3, pub4, pub5
-    forvard = 2
-    back = -2
+    forvard = 3
+    back = -3
     right = -2
     left = 2
 
