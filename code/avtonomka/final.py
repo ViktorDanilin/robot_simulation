327440cd19267e7061710e15e9b8e463cfdd72eb
ViktorDanilin <danilinvs58@gmail.com>
2020-04-30 01:08:55 +0700

    map

--- code/avtonomka/final.py
+++ code/avtonomka/final.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import rospy
-
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 
+rospy.init_node("final")
+goal_publisher = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=5)
